544c48f380082a1d45655347704cb9d91f92a661
root <gitongasamuel368@gmail.com>
2023-09-12 09:18:12 -0700

    alphabet

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,8 @@
  * main function entry point
  * Return: 0
  */
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char alphab;
+
+for (alphab = 'a'; alphab <= 'z'; alphab++)
+_putchar(alphab);
+_putchar('\n');
+}

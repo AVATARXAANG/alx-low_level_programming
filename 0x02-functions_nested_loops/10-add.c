f297159392677610e7996d9de594d6c16ae1b547
root <gitongasamuel368@gmail.com>
2023-09-12 09:59:00 -0700

    0. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two values
+ * @n1: The first num to be computed
+ * @n2: The first num to be computed
+ * Return: interger value
+ */
+
+int add(int n1, int n2)
+
+{
+	return (n1 + n2);
+
+}
